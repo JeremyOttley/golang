b697c45ec39160ad17cad17a6e2e2be26b2fa6f4
Jeremy Ottley <Gazaunga@gmail.com>
2023-10-18 12:01:43 -0400

    Create renamer.go

---
+++ renamer.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "io/ioutil"
+    "path/filepath"
+    "math/rand"
+    "time"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const randomStringLen = 16
+
+func main() {
+    rand.Seed(time.Now().UnixNano())
+    files, err := ioutil.ReadDir(".")
+    if err != nil {
+        fmt.Println("Error reading the current directory:", err)
+        return
+    }
+
+    for _, file := range files {
+        if file.IsDir() {
+            continue
+        }
+
+        // Check if the file has a supported image extension.
+        ext := filepath.Ext(file.Name())
+        if isImageExtension(ext) {
+            newName := generateRandomString() + ext
+            err := os.Rename(file.Name(), newName)
+            if err != nil {
+                fmt.Println("Error renaming file:", err)
+            } else {
+                fmt.Printf("Renamed %s to %s\n", file.Name(), newName)
+            }
+        }
+    }
+}
+
+func isImageExtension(ext string) bool {
+    // Add more image extensions if needed.
+    imageExtensions := []string{".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff"}
+    for _, imgExt := range imageExtensions {
+        if ext == imgExt {
+            return true
+        }
+    }
+    return false
+}
+
+func generateRandomString() string {
+    b := make([]byte, randomStringLen)
+    for i := range b {
+        b[i] = letterBytes[rand.Intn(len(letterBytes))]
+    }
+    return string(b)
+}
